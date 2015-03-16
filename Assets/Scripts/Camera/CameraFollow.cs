1b6284fdaa069904c65591d154bdcb935ecf4841
nq.khai <nq.khai@gloops.com>
2015-03-16 10:00:14 +0700

    add enemy and player ac

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	public float smoothing = 5;
+
+	Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate ()
+	{
+		// Create a postion the camera is aiming for based on the offset from the target.
+		Vector3 targetCamPos = target.position + offset;
+		
+		// Smoothly interpolate between the camera's current position and it's target position.
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
